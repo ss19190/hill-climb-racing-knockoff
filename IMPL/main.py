6392b04d0a9da3a0c97f43eb1fd580e0e2c634d5
ss19190 <sarass19190@gmail.com>
2025-06-01 20:06:23 +0200

    use guide

--- IMPL/main.py
+++ IMPL/main.py
@@ -2,7 +2,7 @@ from agent import train_agent, evaluate_agent
 
 if __name__ == "__main__":
     # Train the agent
-    # train_agent(episodes=5000, render=False)
+    train_agent(episodes=5000, render=False)
 
     # Evaluate the agent
-    evaluate_agent(episodes=10, render=True)
+    # evaluate_agent(episodes=10, render=True)
